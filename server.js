8ad9680d8e74a94404a90f05c103be9d20a2f77c
Felipe Avella <DevJavOps@outlook.com>
2025-09-09 16:44:18 +0000

    Express Web Server v1.0

--- server.js
+++ server.js
@@ -3,13 +3,43 @@ const path = require('path');
 const app = express();
 const PORT = 3500;
 
-// Serve index.html at the root URL
+app.use(express.json()); // For parsing JSON bodies
+
+// GET
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Optionally serve static files (CSS, JS, images)
-app.use(express.static(__dirname));
+app.get('/about', (req, res) => {
+  res.send('This is the About page.');
+});
+
+// POST
+app.post('/submit', (req, res) => {
+  res.json({ message: 'Form submitted!', data: req.body });
+});
+
+app.post('/login', (req, res) => {
+  res.json({ message: 'Login successful!', user: req.body.username });
+});
+
+// PUT
+app.put('/update-profile', (req, res) => {
+  res.json({ message: 'Profile updated!', data: req.body });
+});
+
+app.put('/change-password', (req, res) => {
+  res.json({ message: 'Password changed!' });
+});
+
+// DELETE
+app.delete('/delete-account', (req, res) => {
+  res.json({ message: 'Account deleted!' });
+});
+
+app.delete('/remove-item', (req, res) => {
+  res.json({ message: 'Item removed!' });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
